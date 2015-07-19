aeb1ba0033c9c6d2a4cb1eec7a8ae5b7eae10c5c
Peter Dave Hello <hsu@peterdavehello.org>
2015-07-20 03:40:05 +0800

    use npm package "mkdirp" instead of self-made utils

--- autoupdate.js
+++ autoupdate.js
@@ -3,13 +3,13 @@
 //https://github.com/lodash/lodash/tree/2.4.1
 var _ = require('lodash');
 var fs = require('fs');
-var utils = require('./utils');
 var colors = require('colors');
 var async = require('async');
 var config = require('./config');
 var rimraf = require('rimraf');
 var glob = require('glob');
 var _ = require('lodash');
+var mkdirp = require('mkdirp');
 var TEMP_FOLDER = config.TEMP_FOLDER;
 
 var gitUpdater = require('./updaters/git')
@@ -40,29 +40,33 @@ var startAutoUpdate = function(package, callback) {
 	}
 }
 
-var initialize = function () {
-	console.log('Starting Auto Update'.cyan)
-	console.log('-----------------------');
-	var filenames = glob.sync(__dirname + "/../cdnjs/ajax/libs/*/package.json");
-	var packages = _.chain(filenames)
-		.map(function(filename) {
-			return JSON.parse(fs.readFileSync(filename, 'utf8'))
-		})
-		.filter(function(package) {
-			return typeof package.autoupdate === 'object';
-		})
-		.value();
-
-	async.eachLimit(packages, 16, function (package, callback) {
-			startAutoUpdate(package, callback);
-
-	}, function () {
-		console.log('\n');
+var initialize = function (err) {
+	if (!err) {
+		console.log('Starting Auto Update'.cyan)
 		console.log('-----------------------');
+		var filenames = glob.sync(__dirname + "/../cdnjs/ajax/libs/*/package.json");
+		var packages = _.chain(filenames)
+			.map(function(filename) {
+				return JSON.parse(fs.readFileSync(filename, 'utf8'))
+			})
+			.filter(function(package) {
+				return typeof package.autoupdate === 'object';
+			})
+			.value();
 
-		console.log('Auto Update Completed'.green)
-	});
+		async.eachLimit(packages, 16, function (package, callback) {
+				startAutoUpdate(package, callback);
+
+		}, function () {
+			console.log('\n');
+			console.log('-----------------------');
+
+			console.log('Auto Update Completed'.green)
+		});
+	} else {
+		console.error("Got an error: " + err);
+	}
 }
 
 rimraf.sync(TEMP_FOLDER);
-utils.ensureExists(TEMP_FOLDER, initialize);
+mkdirp(TEMP_FOLDER, initialize);
