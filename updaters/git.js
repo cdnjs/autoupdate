c91a97c919ade50882f391e0a2acce2661251e65
Peter Dave Hello <hsu@peterdavehello.org>
2016-04-06 20:04:49 +0800

    updaters/git.js coding style

--- updaters/git.js
+++ updaters/git.js
@@ -11,39 +11,39 @@ var config = require('../config'),
   stable = require('semver-stable'),
   compareVersions = require('compare-versions');
 
-var update = function (package, callback) {
+var update = function(package, callback) {
   var target = package.autoupdate.target;
   console.log('Clone', target);
   git.clone(target, TEMP_FOLDER + package.name, function(err, repo) {
     if (err) {
       throw err;
     }
-    repo.tags(function(err, tags){
-      var versions = _.map(tags, function(tag){ return tag.name});
-      var needed = _.filter(versions, function(version){
-        if(version[0] === 'v' || version[0] === 'V') {
+    repo.tags(function(err, tags) {
+      var versions = _.map(tags, function(tag) {return tag.name});
+      var needed = _.filter(versions, function(version) {
+        if (version[0] === 'v' || version[0] === 'V') {
           version = version.substr(1);
         }
-        if(!cdnjs.checkVersion(package, version)) {
+        if (!cdnjs.checkVersion(package, version)) {
           return true;
         } else {
           return false;
         }
       });
-      if(needed.length > 0){
+      if (needed.length > 0) {
         console.log('Need', needed.join(',').blue);
       }
-      async.eachSeries(needed, function (tag, callback) {
-          repo.checkout(tag, function () {
-            if(tag[0] === 'v' || tag[0] === 'V') {
+      async.eachSeries(needed, function(tag, callback) {
+          repo.checkout(tag, function() {
+            if (tag[0] === 'v' || tag[0] === 'V') {
               tag = tag.substr(1);
             }
             var basePath = package.autoupdate.basePath || "",
               libContentsPath = path.normalize(path.join(TEMP_FOLDER, package.name, basePath)),
                allFiles = [];
-            _.each(package.autoupdate.files, function (file){
-               var files = glob.sync(path.normalize(path.join(libContentsPath, file)), {nodir:true, realpath:true});
-               if(files.length ===0) {
+            _.each(package.autoupdate.files, function(file) {
+               var files = glob.sync(path.normalize(path.join(libContentsPath, file)), {nodir: true, realpath: true});
+               if (files.length === 0) {
                 console.log('Not found'.red, file.cyan, tag);
                 fs.mkdirsSync(path.normalize(path.join(__dirname, '../../cdnjs', 'ajax', 'libs', package.name, tag)));
                };
@@ -51,26 +51,26 @@ var update = function (package, callback) {
 
             });
             console.log('All files for this version', allFiles.length);
-              console.log(allFiles.length, allFiles.length !==0)
+              console.log(allFiles.length, allFiles.length !== 0)
             package.version = package.version || "0.0.0";
-            if(allFiles.length !==0 && stable.is(tag) && compareVersions(tag, package.version) > 0){
+            if (allFiles.length !== 0 && stable.is(tag) && compareVersions(tag, package.version) > 0) {
               console.log('Updated package.json to version'.green, tag);
               var packagePath = path.normalize(path.join(__dirname, '../../cdnjs', 'ajax', 'libs', package.name, 'package.json')),
                 packageJSON = JSON.parse(fs.readFileSync(packagePath, 'utf8'));
               packageJSON.version = tag;
               fs.writeFileSync(packagePath, JSON.stringify(packageJSON, undefined, 2)  + '\n');
             } 
-            async.eachSeries(allFiles, function(file, callback){
+            async.eachSeries(allFiles, function(file, callback) {
               var fileName = path.relative(path.join(TEMP_FOLDER, package.name, basePath), file),
                 fileTarget = path.normalize(path.join(__dirname, '../../cdnjs', 'ajax', 'libs', package.name, tag, fileName));
               fs.ensureFile(fileTarget, function(err) {
-                if(err){
+                if (err) {
                   console.log('Some strange error occured here'.red);
                   console.dir(err);
                   callback();
                 } else {
-                  fs.copy(file, fileTarget, function (err) {
-                    if(err){
+                  fs.copy(file, fileTarget, function(err) {
+                    if (err) {
                       console.dir(err);
                       console.log('Some strange error occured here'.red);
                       callback();
@@ -80,12 +80,12 @@ var update = function (package, callback) {
                   })
                 }
               })
-            }, function () {
+            }, function() {
               callback();
             })
           })
 
-      }, function () {
+      }, function() {
         console.log('Updated from Git'.green);
         fs.removeSync(TEMP_FOLDER + package.name);
         callback(null, 1);
