932a62db2f4a7fb2b5f66c6bc8c98dcc80291f82
Peter Dave Hello <hsu@peterdavehello.org>
2016-11-03 14:37:12 +0800

    support running auto-update on certain lib, fix #5

--- autoupdate.js
+++ autoupdate.js
@@ -28,7 +28,9 @@ var initialize = function(err) {
   } else {
     console.log('Starting Auto Update'.cyan);
     console.log('-----------------------');
-    var filenames = glob.sync(__dirname + "/../cdnjs/ajax/libs/*/package.json");
+    var args = process.argv.slice(2);
+    var globPattern = (args.length === 1) ? args[0] : '*';
+    var filenames = glob.sync(__dirname + "/../cdnjs/ajax/libs/" + globPattern + "/package.json");
     var librarys = _.chain(filenames)
       .map(function(filename) {
         return JSON.parse(fs.readFileSync(filename, 'utf8'));
